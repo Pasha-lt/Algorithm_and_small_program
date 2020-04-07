3e6d0140f5acb8acdbe0034a1937ff45cf40b909
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2020-04-07 22:53:15 +0300

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -26,3 +26,36 @@ if x == False: #проверка если числа нет то пишем чт
     print('Item', iskat, 'Not found!')
 else: # иначе пишем его индекс(если нашли).
     print('Item', iskat, 'Found at index', x)
+    
+ exit()
+
+#from book
+
+def binary_search(list, item):
+  # low and high keep track of which part of the list you'll search in.
+  low = 0
+  high = len(list) - 1
+
+  # While you haven't narrowed it down to one element ...
+  while low <= high:
+    # ... check the middle element
+    mid = (low + high) // 2
+    guess = list[mid]
+    # Found the item.
+    if guess == item:
+      return mid
+    # The guess was too high.
+    if guess > item:
+      high = mid - 1
+    # The guess was too low.
+    else:
+      low = mid + 1
+
+  # Item doesn't exist
+  return None
+
+my_list = [1, 3, 5, 7, 9]
+print(binary_search(my_list, 3)) # => 1
+
+# 'None' means nil in Python. We use to indicate that the item wasn't found.
+print(binary_search(my_list, -1)) # => None
