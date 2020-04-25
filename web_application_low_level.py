fd6e763a4b51fa3de699a33ac79e94ce07013e16
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2020-04-25 14:29:09 +0300

    Update web_application_low_level.py

--- web_application_low_level.py
+++ web_application_low_level.py
@@ -2,7 +2,7 @@ import socket
 
 
 URLS = {
-    '/': 'hello index', # Для корневого будет ответ hello index
+    '/': 'hello index', # Для главной страницы будет ответ hello index
     '/blog': 'hello blog',
 }
 
@@ -15,10 +15,10 @@ def parse_request(request):
 
 
 def generate_headers(method, url):
-    if not method == 'GET': #Если метод не GET
+    if not method == 'GET': # Если метод не GET
         return ('HTTP/1.1 405 Method not allowed\n\n', 405)
 
-    if not url in URLS: #будем использовать словарь для хранения урлов и если его нет у нас возвращаем 404
+    if not url in URLS: # Будем использовать словарь для хранения урлов и если его нет у нас возвращаем 404
         return ('HTTP/1.1 404 Not found\n\n', 404)
 
     else:
@@ -34,17 +34,17 @@ def generate_content(code, url):
 
 
 def generate_response(request):
-    method, url = parse_request(request) #Распарсим наш реквест, получим шттп метод и юрл запрос.
-    headers, code = generate_headers(method, url) #Тут у нас певым будет заголовок вторым код запроса
-    body = generate_content(code, url)    # Генерация тела ответа
+    method, url = parse_request(request) # Распарсим наш реквест, получим http метод и url запрос.
+    headers, code = generate_headers(method, url) # Тут у нас певым будет заголовок вторым код запроса.
+    body = generate_content(code, url)    # Генерация тела ответа.
     return (headers + body).encode()
 
 
 def run():
     '''создаем субьект который будет принимать запрос'''
-    # AF(adress family) INET(это ip4), ip6 = INET6. socket.SOCK_STREAM - указывает на то что мы берем TCP
+    # AF(adress family) INET(это ip4), ip6 = INET6. socket.SOCK_STREAM - указывает на то что мы берем TCP.
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # 1-й обращаемся к нашему сокету 2-й повторно разрешаем использование сокет и пишем 3-й True(1)
+    # 1-й обращаемся к нашему сокету 2-й повторно разрешаем использование сокет и пишем 3-й True(1).
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Связываем наш субьект с конкретным сервером и портом 127.0.0.1 = localhost. можно было написать и localhost
     server_socket.bind(('127.0.0.1', 5000))
