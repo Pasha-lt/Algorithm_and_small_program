2baf3b8b896a1e52f185433444bbf0543515f5b7
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2021-10-17 16:25:52 +0300

    Update create_base_with_proxy.py

--- create_base_with_proxy.py
+++ create_base_with_proxy.py
@@ -3,21 +3,40 @@ from bs4 import BeautifulSoup
 import re
 
 
-HEADERS = {
-    'User-Agent': 'user_agent'
-}
-URL = 'site'
-response = requests.get(URL, headers=HEADERS)
-soup = BeautifulSoup(response.content, 'html.parser')
-items = soup.findAll('tr', class_='spy1xx')
-items_2 = soup.findAll('tr', class_='spy1x')
-items = str(items) + str(items_2)
+import requests
+from bs4 import BeautifulSoup
+import re
 
-ip_list_with_scum = items.split('class')
-ip_list = []
-for in_str in ip_list_with_scum:
-    m = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:\:\d{1,5})?)', in_str)
-    if m:
-        ip_list.append(m.group(0))
 
-print(ip_list)
+def check_proxy(list_proxy):
+    work_proxy = []
+    for number, proxy in enumerate(list_proxy):
+        url = 'http://' + proxy
+        try:
+            response = requests.get('https://www.google.com/', proxies={'http': url})
+            if response.status_code == 200:
+                # print(f'{url} рабочий юрл {number}')
+                work_proxy.append(proxy)
+        except requests.exceptions.ConnectionError:
+            print(url, 'нерабочий юрл')
+            continue
+    return work_proxy
+
+def get_ip_list():
+    HEADERS = {
+        'User-Agent': 'user agent'
+    }
+    URL = 'url'
+    response = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    items = soup.findAll('tr', class_='spy1xx')
+    items_2 = soup.findAll('tr', class_='spy1x')
+    items = str(items) + str(items_2)
+    
+    ip_list_with_scum = items.split('class')
+    ip_list = []
+    for in_str in ip_list_with_scum:
+        m = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:\:\d{1,5})?)', in_str)
+        if m:
+            ip_list.append(m.group(0))
+    return check_proxy(ip_list)
