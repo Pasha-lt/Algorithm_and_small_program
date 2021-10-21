259f1efddf563616a4dfab5bf9cdf4dc34659a47
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2021-10-21 22:33:22 +0300

    Update create_base_with_proxy.py

--- create_base_with_proxy.py
+++ create_base_with_proxy.py
@@ -4,24 +4,24 @@ import re
 
 
 def check_proxy(list_proxy):
-    """Метод проверяет рабочее ли прокси. на вход принимает лист со всеми прокси, возвращает лист
+    """Метод проверяет рабочее ли прокси на вход принимает лист со всеми прокси возвращает лист
     с рабочими прокси"""
     work_proxy = []  # создаем пустой список куда будем наполнять рабочие прокси
     # проходим списком по листу всех прокси, чтобы определить какие рабочие какие нет.
     for number, proxy in enumerate(list_proxy):
-         # todo нужно переделать чтобы это был список c двумявариантами и передавает его как значение.
-        url = 'https://' + proxy  # Добавляем к нашему прокси http или https
         try:
-            # Отправляем запрос через прокси и сохраняем его в переменую response
-            response = requests.get('https://www.google.com/', proxies={'http': url})
-            # проверяем статус код ответа если он 200 то добавляем в наш список прокси
+            # Отправляем запрос через прокси и сохраняем его в переменую respone  
+            response = requests.get('https://www.google.com/',
+                                    proxies={'http': proxy, 'https': proxy}, timeout=5)
+            # проверяем статус код ответа если он 200 то добавляем в нас список прокси
             if response.status_code == 200:
                 work_proxy.append(proxy)
         except requests.exceptions.ConnectionError:
             # если мы поймали исключение тогда юрл не сохраняем
-            print(url, 'нерабочий юрл')
+            print(proxy, 'нерабочий юрл')
             continue
-    return work_proxy # возвращаем список рабочих юрлов.
+    return work_proxy  # возвращаем список рабочих юрлов.
+
 
 def get_ip_list():
     """функция парсит сайт и достает с него все proxy"""
@@ -30,7 +30,7 @@ def get_ip_list():
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
     }
     
-    URL = 'https://'  # юрл сайта который мы будем парсить
+    URL = ''  # юрл сайта который мы будем парсить
     # отправляем запрос на сайт, сохраняем ответ в переменую что бы в дальнейшем его спарсить.
     response = requests.get(URL, headers=HEADERS)
     soup = BeautifulSoup(response.content, 'html.parser')
